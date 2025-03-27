c289bce774101b2041639bc78b2998f7f63e7fb0
Dmitrii Bogorodskii <DmitriiBoog@gmail.com>
2025-03-26 21:37:12 -0400

    finish assignment2

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -77,3 +77,74 @@ def all_employees_dict():
         result[employee_id] = employee_dict(row)
     return result
 
+print(all_employees_dict())
+# Questions 2 to 9 are like an Excel table, so pretty simple. Though, again, there are a lot of new information, so brain is going crazy))
+
+#Q10
+import os
+
+def get_this_value():
+    return os.environ.get("THISVALUE")
+
+#Q11 
+import custom_module
+
+def set_that_secret(new_secret):
+    custom_module.set_secret(new_secret)
+
+set_that_secret("fire boll")
+print(custom_module.secret)
+
+#Q12
+def read_minutes():
+    def read_file(filename):
+        with open(filename, "r") as file:
+            reader = csv.reader(file)
+            fields = next(reader)
+            rows = [tuple(row) for row in reader]
+        return {"fields": fields, "rows": rows}
+
+    minutes1 = read_file("../csv/minutes1.csv")
+    minutes2 = read_file("../csv/minutes2.csv")
+    return minutes1, minutes2
+minutes1, minutes2 = read_minutes() # forget about it first time ))
+
+# The structure is similar to how employees before.
+#  Are we convert it to a tuple so that it can be put into a set? I'm still a bit slow and confused with this.
+
+#Q13
+def create_minutes_set():
+    set1 = set(minutes1["rows"])
+    set2 = set(minutes2["rows"])
+    return set1.union(set2)
+
+minutes_set = create_minutes_set()
+print(minutes_set)
+
+#Q14
+from datetime import datetime
+
+def create_minutes_list():
+    as_list = list(minutes_set)
+    converted = list(map(lambda x: (x[0], datetime.strptime(x[1], "%B %d, %Y")), as_list))
+    return converted
+
+minutes_list = create_minutes_list()
+print(minutes_list)
+
+#Q15
+def write_sorted_list():
+    sorted_list = sorted(minutes_list, key=lambda x: x[1])
+
+    formatted = list(map(lambda x: (x[0], x[1].strftime("%B %d, %Y")), sorted_list))
+
+    with open("minutes.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(minutes1["fields"]) 
+        writer.writerows(formatted)  
+
+    return formatted
+
+write_sorted_list()
+# Q14-15 is challenge. I understand the logic and how it should be. It's just the syntax that's new, I think.
+# Actually, if we ignore the new syntax and its rules, it's pretty simple and intuitively clear.
---
+++ assignment2/custom_module.py
@@ -0,0 +1,7 @@
+secret = "shazam!"
+
+def set_secret(new_secret):
+    global secret
+    secret = new_secret
+
+#shazam is cool character but i love black adam more ))
